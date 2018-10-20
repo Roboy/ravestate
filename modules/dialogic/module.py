1d99dfe1eaf2312d559ef0a45aff0481f7a6a432
Joseph Birkner <joseph.birkner@campus.lmu.de>
2018-10-20 20:03:01 +0200

    Added module registry and rawio, consoleio modules.

--- modules/dialogic/module.py
+++ modules/dialogic/module.py
@@ -2,7 +2,13 @@
 
 
 class Module:
+    """
+    Atomic class, which encapsulates a named set of states, properties and config entries,
+    which form a coherent bundle.
+    """
 
-    def __init__(self, name: str):
+    def __init__(self, *, name: str, props=(), states=(), config={}):
         self.name = name
+        self.props = props
+        self.states = states
 
