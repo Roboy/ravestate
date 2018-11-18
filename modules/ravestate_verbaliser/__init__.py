33e74851a3bd7321b3eecd160cbe9d69e6e6fa30
Andreas Dolp <toseban@gmail.com>
2018-11-18 15:27:12 +0100

    added verbaliser_prop:phrase_intent property

--- modules/ravestate_verbaliser/__init__.py
+++ modules/ravestate_verbaliser/__init__.py
@@ -1,20 +1,28 @@
-from os.path import dirname, realpath, join
+import logging
 
 from ravestate import registry
+from ravestate.property import PropertyBase
 from ravestate.state import state
 from ravestate_verbaliser import verbaliser
 
-
-# TODO this is just a testdummy for a verbaliser state
-verbaliser.add_folder(join(dirname(dirname(dirname(realpath(__file__)))), "resources", "phrase_lists"))
-
-@state(read="rawio:in", write="rawio:out")
-def react_to_input(ctx):
-    raw_in = ctx["rawio:in"]
-    if "hi" in raw_in:
-        ctx["rawio:out"] = verbaliser.get_random_phrase('greetings')
-    elif "joke" in raw_in:
-        ctx["rawio:out"] = verbaliser.get_random_phrase('jokes')
+registry.register(name="verbaliser_prop",
+                  props=(PropertyBase(name="phrase_intent", default=""),))
 
 
-registry.register(name="ravestate_verbaliser", states=(react_to_input,))
+@state(read="verbaliser_prop:phrase_intent", write="rawio:out")
+def react_to_intent(ctx):
+    """
+    Looks for intents written to the verbaliser_prop:phrase_intent property and
+    writes a random phrase for that intent to rawio:out
+    """
+    intent = ctx["verbaliser_prop:phrase_intent"]
+    phrase = verbaliser.get_random_phrase(intent)
+    if phrase:
+        ctx["rawio:out"] = phrase
+    else:
+        logging.error('No phrase for intent ' + intent + ' found.')
+
+
+# there are two seperate registrations because the signals have to be registered
+# before being used in react_to_intent
+registry.register(name="verbaliser", states=(react_to_intent,))
