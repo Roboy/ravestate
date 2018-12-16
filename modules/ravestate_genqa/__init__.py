2dce06a318ba93a96c2949a82106558b4ba04de0
NeginsCode <neginikarimi@gmail.com>
2018-12-16 21:34:56 +0100

    Added random answering intro phrase

--- modules/ravestate_genqa/__init__.py
+++ modules/ravestate_genqa/__init__.py
@@ -1,6 +1,8 @@
 from ravestate import registry
 from ravestate.state import state
 import ravestate_rawio
+from ravestate_verbaliser import verbaliser
+import ravestate_phrases_basic_en
 from ravestate.constraint import s
 
 import logging
@@ -10,11 +12,14 @@ import requests
 def hello_world(ctx):
     ctx["rawio:out"] = "Ask me anything!"
 
-@state(read="rawio:in", write="rawio:out")
+@state(read="rawio:in", write=("rawio:out"))
 def drqa_module(ctx):
     params = {'question': ctx["rawio:in"]}
+    #ctx["verbaliser:intent"] = "question-answering-starting-phrases"
+    answer_phrase = verbaliser.get_random_phrase("question-answering-starting-phrases")
     response = requests.get('http://localhost',params=params)
-    ctx["rawio:out"] = response.json()
+    response_json = response.json()
+    ctx["rawio:out"] = answer_phrase + " " + response_json["answers"][0]["span"]
 
 registry.register(
     name="genqa",
