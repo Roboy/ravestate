59495cc52128bea4b15ad313d07c622e53ca150c
NeginsCode <neginikarimi@gmail.com>
2018-12-17 14:13:27 +0100

    drqa server url moved to config file

--- modules/ravestate_genqa/__init__.py
+++ modules/ravestate_genqa/__init__.py
@@ -8,22 +8,33 @@ from ravestate.constraint import s
 import logging
 import requests
 
-drqa_server_url = 'http://localhost'
+DRQA_SERVER_ADDRESS: str = "drqa_server_address"
 best_answer = 0
 
 @state(triggers=s(":startup"), write="rawio:out")
 def hello_world_genqa(ctx):
     ctx["rawio:out"] = "Ask me anything!"
+    server = ctx.conf(key=DRQA_SERVER_ADDRESS)
+    if not server:
+        logging.error('Server address is not set. Shutting down GenQA.')
+        return Delete()
 
 @state(read="rawio:in", write=("rawio:out"))
 def drqa_module(ctx):
     params = {'question': ctx["rawio:in"]}
     answer_phrase = verbaliser.get_random_phrase("question-answering-starting-phrases")
-    response = requests.get(drqa_server_url, params=params)
+    server = ctx.conf(key=DRQA_SERVER_ADDRESS)
+    if not server:
+        logging.error('Server address is not set. Shutting down GenQA.')
+        return Delete()
+    response = requests.get(server, params=params)
     response_json = response.json()
     ctx["rawio:out"] = answer_phrase + " " + response_json["answers"][best_answer]["span"]
 
 registry.register(
     name="genqa",
-    states=(hello_world_genqa, drqa_module)
+    states=(hello_world_genqa, drqa_module),
+    config={
+        DRQA_SERVER_ADDRESS: ""
+    }
 )
