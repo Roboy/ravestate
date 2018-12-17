30ff4de6f6141680c780fe1d76056669d7323a13
NeginsCode <neginikarimi@gmail.com>
2018-12-17 13:02:35 +0100

    extracting hard coded values to variables

--- modules/ravestate_genqa/__init__.py
+++ modules/ravestate_genqa/__init__.py
@@ -8,20 +8,22 @@ from ravestate.constraint import s
 import logging
 import requests
 
+drqa_server_url = 'http://localhost'
+best_answer = 0
+
 @state(triggers=s(":startup"), write="rawio:out")
-def hello_world(ctx):
+def hello_world_genqa(ctx):
     ctx["rawio:out"] = "Ask me anything!"
 
 @state(read="rawio:in", write=("rawio:out"))
 def drqa_module(ctx):
     params = {'question': ctx["rawio:in"]}
-    #ctx["verbaliser:intent"] = "question-answering-starting-phrases"
     answer_phrase = verbaliser.get_random_phrase("question-answering-starting-phrases")
-    response = requests.get('http://localhost',params=params)
+    response = requests.get(drqa_server_url, params=params)
     response_json = response.json()
-    ctx["rawio:out"] = answer_phrase + " " + response_json["answers"][0]["span"]
+    ctx["rawio:out"] = answer_phrase + " " + response_json["answers"][best_answer]["span"]
 
 registry.register(
     name="genqa",
-    states=(hello_world, drqa_module)
+    states=(hello_world_genqa, drqa_module)
 )
