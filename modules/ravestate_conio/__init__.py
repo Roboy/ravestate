9f1dcc25d0b0c0296eee5dfab8162f4b83a2058d
Andreas Dolp <toseban@gmail.com>
2019-01-09 03:49:26 +0100

    Implement interloc and push interlocs from console and telegram. Interlocs are scientio Nodes

--- modules/ravestate_conio/__init__.py
+++ modules/ravestate_conio/__init__.py
@@ -1,28 +1,71 @@
-
 from ravestate import registry
 from ravestate.constraint import s
+from ravestate.property import PropertyBase
 from ravestate.state import state
 from ravestate.receptor import receptor
+from ravestate.wrappers import ContextWrapper
+
 import ravestate_rawio
+import ravestate_interloc
+from ravestate_verbaliser.verbaliser import get_phrase_list
+import ravestate_phrases_basic_en
+import ravestate_ontology
+
+from scientio.ontology.node import Node
+from scientio.session import Session
+from scientio.ontology.ontology import Ontology
+
+from reggol import get_logger
+logger = get_logger(__name__)
 
 
-@state(read="rawio:in")
-def console_shutdown(ctx):
-    rawin = ctx["rawio:in"]
-    if "bye" in rawin:
-        ctx.shutdown()
-
-
-@state(cond=s(":startup"))
-def console_input(ctx):
+@state(cond=s(":startup"), read="interloc:all")
+def console_input(ctx: ContextWrapper):
 
     @receptor(ctx_wrap=ctx, write="rawio:in")
     def write_console_input(ctx_input, value: str):
         ctx_input["rawio:in"] = value
 
+    @receptor(ctx_wrap=ctx, write="interloc:all")
+    def push_console_interloc(ctx: ContextWrapper, console_node: Node):
+        if ctx.push(parentpath="interloc:all", child=PropertyBase(name='x', default=console_node)):
+            logger.debug(f"Pushed {console_node} to interloc:all")
+
+    @receptor(ctx_wrap=ctx, write="interloc:all")
+    def pop_console_interloc(ctx: ContextWrapper):
+        if ctx.pop("interloc:all:x"):
+            logger.debug(f"Popped interloc:all:x")
+
     while not ctx.shutting_down():
         input_value = input("> ")
         write_console_input(input_value)
+
+        console_interloc_exists = "interloc:all:x" in ctx.enum("interloc:all")
+        # push Node if you got a greeting
+        if input_value.strip() in get_phrase_list("greeting") and not console_interloc_exists:
+            # set up scientio
+            sess: Session = ravestate_ontology.get_session()
+            onto: Ontology = ravestate_ontology.get_ontology()
+
+            # create scientio Node of type Person
+            query = Node(metatype=onto.get_type("Person"))
+            query.set_name("x")
+            console_node_list = sess.retrieve(query)
+            if not console_node_list:
+                console_node = sess.create(query)
+                logger.info(f"Created new Node in scientio session: {console_node}")
+            elif len(console_node_list) == 1:
+                console_node = console_node_list[0]
+            else:
+                logger.error('Found multiple Persons with name x in scientio session. Cannot push node to interloc:all!')
+                continue
+
+            # push interloc-Node
+            push_console_interloc(console_node)
+
+        # pop Node if you got a farewell
+        elif input_value.strip() in get_phrase_list("farewells") and console_interloc_exists:
+            pop_console_interloc()
 
 
 @state(read="rawio:out")
@@ -32,5 +75,5 @@ def console_output(ctx):
 
 registry.register(
     name="consoleio",
-    states=(console_shutdown, console_input, console_output)
+    states=(console_input, console_output)
 )
--- modules/ravestate_interloc/__init__.py
+++ modules/ravestate_interloc/__init__.py
@@ -1,41 +1,4 @@
 from ravestate.property import PropertyBase
-from ravestate.state import state
 from ravestate import registry
 
-import ravestate_verbaliser
-import ravestate_phrases_basic_en
-
-from ravestate.constraint import s
-from ravestate.wrappers import ContextWrapper
-
-from reggol import get_logger
-logger = get_logger(__name__)
-
-
-@state(read="rawio:in", write="interloc:all")
-def push_interloc(ctx: ContextWrapper):
-    if ctx["rawio:in"].strip() in ['hi', 'hello']:
-        ctx.push(parentpath="interloc:all", child=PropertyBase(name='x'))
-        logger.debug(f"Pushed x to interloc:all")
-
-
-@state(triggers=s("interloc:all:pushed"), write="verbaliser:intent")
-def react_to_pushed_interloc(ctx: ContextWrapper):
-    ctx["verbaliser:intent"] = "greeting"
-
-
-@state(read="rawio:in", write="interloc:all")
-def pop_interloc(ctx: ContextWrapper):
-    # for now 'see you' instead of bye because of shutdown, when bye is sent
-    if ctx["rawio:in"].strip() in ['see you']:
-        ctx.pop(path="interloc:all:x")
-        logger.debug(f"Popped interloc:all:x")
-
-
-@state(triggers=s("interloc:all:popped"), write="verbaliser:intent")
-def react_to_popped_interloc(ctx: ContextWrapper):
-    ctx["verbaliser:intent"] = "farewells"
-
-
-registry.register(name="interloc", states=(push_interloc, react_to_pushed_interloc, pop_interloc, react_to_popped_interloc),
-                  props=PropertyBase(name="all"))
+registry.register(name="interloc", props=PropertyBase(name="all"))
