965e464fec3c561adc51cea2f9d39cfd50991ae7
NeginsCode <neginikarimi@gmail.com>
2019-01-28 23:28:24 +0100

    maps multiple synonyms of yes, no, probably....

--- modules/ravestate_nlp/yes_no.py
+++ modules/ravestate_nlp/yes_no.py
@@ -1,24 +1,30 @@
-NEGATION_SET = {"neg"}
+NEGATION_TUPLE = ["neg", "not"]
+YES_SYNONYMS = {"yes", "y", "yeah", "sure", "definitely", "	certainly"}
+NO_SYNONYMS = {"no", "n", "nope", "negative", "never", "nay"}
+PROBABLY_SYNONYMS = {"probably", "likely", "possibly", "perhaps", "maybe"}
+DON_NOT_KNOW_SET = {"know", "understand", "idea"}
 
 
 def yes_no(doc):
     """
-    checks input for "yes", "no", "i don't know", "probably" and "probably not"
-    TODO: This should also map other inputs like ("(for|not) sure", "definitely, "dont think so"...)
+    checks input for "yes", "no", "i don't know", "probably" and "probably not" and synonyms of these
     """
     nlp_tokens = tuple(str(token.text.lower()) for token in doc)
-    for token in nlp_tokens:
-        if token == "yes" or token == "y":
+    if len(nlp_tokens) == 1:
+        if nlp_tokens[0] in YES_SYNONYMS:
             return "yes"
-        elif token == "no" or token == "n":
+        elif nlp_tokens[0] in NO_SYNONYMS:
             return "no"
-    for token in doc:
-        if token.dep_ in NEGATION_SET:
-            if "know" in nlp_tokens:
-                return "idk"
-            elif "probably" in nlp_tokens:
-                return "pn"
-        elif "probably" in nlp_tokens:
+        elif nlp_tokens[0] in PROBABLY_SYNONYMS:
             return "p"
-    return "0"
 
+    nlp_token_dep = tuple(str(token.dep_) for token in doc)
+    if NEGATION_TUPLE[0] in nlp_token_dep or NEGATION_TUPLE[1] in nlp_tokens:
+        for token in nlp_tokens:
+            if token in PROBABLY_SYNONYMS:
+                return "pn"
+            elif token in YES_SYNONYMS:
+                return "no"
+            elif token in DON_NOT_KNOW_SET:
+                return "idk"
+    return "0"
