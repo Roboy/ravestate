64d09c329e91435d472781b86e6d2c63069cfa25
NeginsCode <neginikarimi@gmail.com>
2019-01-28 22:28:40 +0100

    yes no detection is not case sensitive

--- modules/ravestate_nlp/yes_no.py
+++ modules/ravestate_nlp/yes_no.py
@@ -6,7 +6,7 @@ def yes_no(doc):
     checks input for "yes", "no", "i don't know", "probably" and "probably not"
     TODO: This should also map other inputs like ("(for|not) sure", "definitely, "dont think so"...)
     """
-    nlp_tokens = tuple(str(token) for token in doc)
+    nlp_tokens = tuple(str(token.text.lower()) for token in doc)
     for token in nlp_tokens:
         if token == "yes" or token == "y":
             return "yes"
