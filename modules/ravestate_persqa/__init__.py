76dd89d19561812c143df51c1cf57bbb3f391f14
NeginsCode <neginikarimi@gmail.com>
2019-02-05 22:40:39 +0100

    interlocutor name is saved in node

--- modules/ravestate_persqa/__init__.py
+++ modules/ravestate_persqa/__init__.py
@@ -3,9 +3,13 @@ from ravestate.property import PropertyBase
 from ravestate.state import state, Emit, Delete
 from ravestate.constraint import s
 from ravestate_verbaliser import verbaliser
+import ravestate_ontology
 
 from os.path import realpath, dirname, join
 
+from scientio.ontology.ontology import Ontology
+from scientio.session import Session
+from scientio.ontology.node import Node
 from reggol import get_logger
 logger = get_logger(__name__)
 
@@ -66,15 +70,38 @@ with Module(name="persqa"):
 
     @state(cond=s("nlp:triples:changed"),
            write="persqa:answer",
-           read=("persqa:predicate", "nlp:triples"))
+           read=("persqa:predicate", "nlp:triples", "nlp:tokens"))
     def inference(ctx):
+        """
+        recognizes name in sentences like:
+         - i am jj
+         - my name toseban
+         - dino
+        """
         triple = ctx["nlp:triples"][0]
         if ctx["persqa:predicate"] == "NAME":
-                ctx["persqa:answer"] = triple.get_object() if triple.get_object() else None
+            ctx["persqa:answer"] = triple.get_object()
+            if len(ctx["nlp:tokens"]):
+                ctx["persqa:answer"] = ctx["nlp:tokens"][0]
 
 
     @state(cond=s("persqa:answer:changed"),
            write="rawio:out",
            read=("persqa:predicate", "persqa:subject", "persqa:answer"))
     def react(ctx):
-        ctx["rawio:out"] = verbaliser.get_random_successful_answer(ctx["persqa:predicate"]) % str(ctx["persqa:answer"])
+        name = str(ctx["persqa:answer"])
+        ctx["rawio:out"] = verbaliser.get_random_successful_answer(ctx["persqa:predicate"]) % name
+        sess: Session = ravestate_ontology.get_session()
+        onto: Ontology = ravestate_ontology.get_ontology()
+        query = Node(metatype=onto.get_type("Person"))
+        query.set_properties({"name": name})
+        node_list = sess.retrieve(query)
+        if not node_list:
+            node = sess.create(query)
+        elif len(node_list) == 1:
+            node = node_list[0]
+        else:
+            logger.error("Failed to create node!")
+            return
+        logger.info(f"Interlocutor: Name = {name}; Node ID = {node.get_id()} ")
+
