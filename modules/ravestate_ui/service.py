debc06870d73b6bfa84e4a5da2c23c53d666dad7
Laura Lahesoo <laura.lahesoo@tum.de>
2019-02-08 09:54:56 +0100

    Adjusting GUI to new ravestate structure

--- modules/ravestate_ui/service.py
+++ modules/ravestate_ui/service.py
@@ -1,10 +1,12 @@
 from flask import Flask, url_for, render_template, send_from_directory
 from flask import jsonify
+from flask_socketio import SocketIO, emit
 
 from ravestate import registry
 
 app = Flask(__name__)
 context = None
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -14,12 +16,17 @@ def index():
 @app.route('/data')
 def get_data():
     global context
+    sets = parse_data()
+    return jsonify(sets)
+
+
+def parse_data():
     sets = []
-    for module in registry._registered_modules.items():
-        for state in module[1].states:
+    for name, module in registry._registered_modules.items():
+        for state in module.states:
             for trigger in state.read_props:
                 set = {}
-                set['source'] = " " + str(trigger)
+                set['source'] = str(" " + trigger)
                 set['target'] = state.name
                 set['type'] = 'notifies'
                 sets.append(set)
@@ -29,32 +36,24 @@ def get_data():
                 set['target'] = target
                 set['type'] = 'changes'
                 sets.append(set)
-    # for prop in context._properties.keys():
-    #     for type in context.default_property_signals:
-    #         set = {}
-    #         set['source'] = prop
-    #         set['target'] = "{}{}".format(prop, type)
-    #         set['type'] = 'creates'
-    #         sets.append(set)
+    return sets
 
-    cleanSet = []
-    for set in sets: # Don't display unused signals
-        if set['type'] == 'creates':
-            used = False
-            for compare in sets:
-                if set['target'] == compare['source']:
-                    used = True
-                    break
-            if used:
-                cleanSet.append(set)
-        else:
-            cleanSet.append(set)
-    return jsonify(cleanSet)
 
+# background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print("Hello")
+    return "nothing"
+
+@socketio.on('update')
+def update():
+    sets = parse_data()
+    emit('newdata', jsonify(sets))
 
 def advertise(*, ctx, ip="0.0.0.0", port=5000, debug=False):
     global context
     context = ctx
     app.debug=debug
     app.run(ip, port)
+    socketio.run(app)
     url_for('static', filename='graph.css')
