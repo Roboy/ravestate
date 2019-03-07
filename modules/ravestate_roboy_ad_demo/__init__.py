0980dda562dbd6374c718eaae9d730e2387b69f8
Andreas Dolp <toseban@gmail.com>
2019-03-07 14:58:48 +0100

    adapt for communication with AD

--- modules/ravestate_roboy_ad_demo/__init__.py
+++ modules/ravestate_roboy_ad_demo/__init__.py
@@ -14,20 +14,23 @@ from std_msgs.msg import String
 from reggol import get_logger
 logger = get_logger(__name__)
 
-ROS2_PICKUP_REQUESTED_TOPIC_CONFIG = "ros2-pickup-requested-topic"
-ROS2_START_DRIVING_TOPIC_CONFIG = "ros2-start-driving-topic"
-ROS2_ARRIVED_AT_PICKUP_TOPIC_CONFIG = "ros2-arrived-at-pickup-topic"
-ROS2_ARRIVED_AT_DROPOFF_TOPIC_CONFIG = "ros2-arrived-at-dropoff-topic"
+
+DR_TO_AD_TOPIC = "/dr_to_ad"
+AD_TO_DR_TOPIC = "/ad_to_dr"
+
+PICKUP_REQUESTED_MSG = "pick_up_requested"
+START_DRIVING_MSG = "start_driving"
+
+ARRIVED_AT_PICKUP_POINT_MSG = "arrived_at_pick_up_point"
+ARRIVED_AT_DROPOFF_POINT_MSG = "arrived_at_drop_off_point"
+
+
 ROS2_HANDSHAKE_MOTION_TOPIC_CONFIG = "ros2-handshake-motion-topic"
 HANDSHAKE_MOTION_NAME_CONFIG = "handshake-motion-name"
 
 CONFIG = {
-    ROS2_PICKUP_REQUESTED_TOPIC_CONFIG: "/ros2_pickup_requested", # TODO
-    ROS2_START_DRIVING_TOPIC_CONFIG: "/ros2_start_driving", # TODO
-    ROS2_ARRIVED_AT_PICKUP_TOPIC_CONFIG: "/ros2_arrived_at_pickup", # TODO
-    ROS2_ARRIVED_AT_DROPOFF_TOPIC_CONFIG: "/ros2_arrived_at_dropoff", # TODO
-    ROS2_HANDSHAKE_MOTION_TOPIC_CONFIG: "/roboy_plexus/ReplayTrajectory", # TODO
-    HANDSHAKE_MOTION_NAME_CONFIG: "HandshakeMotion", # TODO
+    ROS2_HANDSHAKE_MOTION_TOPIC_CONFIG: "/roboy_plexus/ReplayTrajectory",  # TODO
+    HANDSHAKE_MOTION_NAME_CONFIG: "HandshakeMotion",  # TODO
 }
 
 with Module(name="ad_demo", config=CONFIG) as mod:
@@ -39,62 +42,53 @@ with Module(name="ad_demo", config=CONFIG) as mod:
     @state(cond=startup(), write=ros2_properties_parent.id())
     def create_ros2_properties(ctx: ContextWrapper):
         # create publishers and subscribers
-        publish_pickup_requested = Ros2PubProperty(name="publish_pickup_requested",
-                                                   topic=ctx.conf(key=ROS2_PICKUP_REQUESTED_TOPIC_CONFIG),
-                                                   msg_type=String)
-        publish_start_driving = Ros2PubProperty(name="publish_start_driving",
-                                                topic=ctx.conf(key=ROS2_START_DRIVING_TOPIC_CONFIG),
-                                                msg_type=String)
-        subscribe_arrived_at_pickup_point = Ros2SubProperty(name="subscribe_arrived_at_pickup_point",
-                                                            topic=ctx.conf(key=ROS2_ARRIVED_AT_PICKUP_TOPIC_CONFIG),
-                                                            msg_type=String)
-        subscribe_arrived_at_dropoff_point = Ros2SubProperty(name="subscribe_arrived_at_dropoff_point",
-                                                             topic=ctx.conf(key=ROS2_ARRIVED_AT_DROPOFF_TOPIC_CONFIG),
-                                                             msg_type=String)
+        publish_dr_to_ad = Ros2PubProperty(name="publish_dr_to_ad",
+                                           topic=DR_TO_AD_TOPIC,
+                                           msg_type=String)
+        subscribe_ad_to_dr = Ros2SubProperty(name="subscribe_ad_to_dr",
+                                             topic=AD_TO_DR_TOPIC,
+                                             msg_type=String)
         publish_handshake_motion = Ros2PubProperty(name="publish_handshake_motion",
                                                    topic=ctx.conf(key=ROS2_HANDSHAKE_MOTION_TOPIC_CONFIG),
                                                    msg_type=String)
         # add the ros2 properties to context
-        ctx.push(ros2_properties_parent.id(), publish_pickup_requested)
-        ctx.push(ros2_properties_parent.id(), publish_start_driving)
-        ctx.push(ros2_properties_parent.id(), subscribe_arrived_at_pickup_point)
-        ctx.push(ros2_properties_parent.id(), subscribe_arrived_at_dropoff_point)
+        ctx.push(ros2_properties_parent.id(), publish_dr_to_ad)
+        ctx.push(ros2_properties_parent.id(), subscribe_ad_to_dr)
         ctx.push(ros2_properties_parent.id(), publish_handshake_motion)
 
         @state(cond=s("nlp:triples:changed"),
-               read="nlp:triples", write=(publish_pickup_requested.id(), raw_out.id()))
+               read="nlp:triples", write=(publish_dr_to_ad.id(), raw_out.id()))
         def pickup_requested(ctx: ContextWrapper):
             # Trigger phrase: "Pick me up."
             triples = ctx["nlp:triples"]
             if triples[0].match_either_lemma(pred={"pick"}):
-                ctx[publish_pickup_requested.id()] = String(data="Pick me up")  # TODO
+                ctx[publish_dr_to_ad.id()] = String(data=PICKUP_REQUESTED_MSG)
                 ctx[raw_out.id()] = "I'm on my way!"
             else:
                 return Resign()
 
-        @state(read=subscribe_arrived_at_pickup_point.id(), write=raw_out.id())
-        def arrived_at_pickup_point(ctx: ContextWrapper):
-            # processes signal from AD
-            # voices: "Hop on!"
-            ctx[raw_out.id()] = "Hop on!"
+        @state(read=subscribe_ad_to_dr.id(), write=raw_out.id())
+        def arrived_somewhere(ctx: ContextWrapper):
+            # processes message from AD
+            msg = ctx[subscribe_ad_to_dr.id()].data
+            if msg == ARRIVED_AT_PICKUP_POINT_MSG:
+                ctx[raw_out.id()] = "Hop on!"
+            elif msg == ARRIVED_AT_DROPOFF_POINT_MSG:
+                ctx[raw_out.id()] = "We have arrived."
+            else:
+                logger.error(f"Unexpected message: {msg} received on topic: {AD_TO_DR_TOPIC}")
 
         @state(cond=s("nlp:triples:changed"),
-               read="nlp:triples", write=(publish_start_driving.id(), raw_out.id()))
+               read="nlp:triples", write=(publish_dr_to_ad.id(), raw_out.id()))
         def start_driving(ctx: ContextWrapper):
             # Trigger phrase: "Start driving." or "Go."
             triples = ctx["nlp:triples"]
             if triples[0].match_either_lemma(pred={"drive", "start", "go"}):
                 # lets go
-                ctx[publish_start_driving.id()] = String(data="lets go")  # TODO
+                ctx[publish_dr_to_ad.id()] = String(data=START_DRIVING_MSG)
                 ctx[raw_out.id()] = "Fasten your seat belt and off we go!"
             else:
                 return Resign()
-
-        @state(read=subscribe_arrived_at_dropoff_point.id(), write=raw_out.id())
-        def arrived_at_drop_off_point(ctx: ContextWrapper):
-            # processes signal from AD
-            # Voices. "We have arrived"
-            ctx[raw_out.id()] = "We have arrived."
 
         @state(cond=raw_out.changed_signal(), write=publish_handshake_motion.id())
         def lennart_recognized(ctx: ContextWrapper):
@@ -102,6 +96,6 @@ with Module(name="ad_demo", config=CONFIG) as mod:
             # triggers handshake: topic ReplayTrajectory which takes a string as a name
             ctx[publish_handshake_motion.id()] = String(data=ctx.conf(key=HANDSHAKE_MOTION_NAME_CONFIG))
 
-        for st in [pickup_requested, start_driving, arrived_at_pickup_point, arrived_at_drop_off_point, lennart_recognized]:
+        for st in [pickup_requested, start_driving, arrived_somewhere, lennart_recognized]:
             mod.add(st)
             ctx.add_state(st)
