0373df765608c08d9bd377fffecd5ccf22d98a60
Laura Lahesoo <laura.lahesoo@tum.de>
2019-03-09 00:17:10 +0100

    UI, kinda pretty.

--- modules/ravestate_ui/service.py
+++ modules/ravestate_ui/service.py
@@ -1,31 +1,26 @@
-from flask import Flask, url_for, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory
 from flask import jsonify
 from flask_socketio import SocketIO, emit
-
-from ravestate.module import Module
+from ravestate.context import Context
 
 app = Flask(__name__, static_url_path='')
 
-context = None
+context: Context = None
 socketio = SocketIO(app)
 
 @app.route('/scripts/<path:path>')
 def send_js(path):
     return send_from_directory('scripts', path)
 
-@app.route('/node_modules/<path:path>')
-def send_node(path):
-    return send_from_directory('node_modules', path)
+@app.route('/static/<path:path>')
+def send_css(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def index():
     global context
     return render_template('index.html')
 
-@app.route('/matter')
-def matter_demo():
-    global context
-    return render_template('matter-demo.html')
 
 @app.route('/data')
 def get_data():
@@ -33,31 +28,58 @@ def get_data():
     sets = parse_data()
     return jsonify(sets)
 
+def format_whitespace(s, sub, nth):
+    find = s.find(sub)
+    # loop util we find no match
+    i = 1
+    while find != -1:
+        # if i  is equal to nth we found nth matches so replace
+        if i == nth:
+            s = s[:find]+"\n"+s[find + len(sub):]
+            i = 0
+        # find + len(sub) + 1 means we start after the last match
+        find = s.find(sub, find + len(sub) + 1)
+        i += 1
+    return s
 
 def parse_data():
+    global context
     sets = []
-    for name, module in Module.registered_modules.items():
-        for state in module.states:
-            for trigger in state.read_props:
-                set = {}
-                set['source'] = str(" " + trigger)
-                set['target'] = state.name
-                set['type'] = 'notifies'
-                sets.append(set)
-            for target in state.write_props:
-                set = {}
-                set['source'] = state.name
-                set['target'] = target
-                set['type'] = 'changes'
-                sets.append(set)
+
+    for state in context._activations_per_state:
+        for target in state.write_props:
+            set = {}
+            # set['source'] = format_whitespace(state.name, "_", 2).replace("_", " ")
+            set['source'] = state.name
+            set['target'] = target
+            set['type'] = 'changes'
+            sets.append(set)
+        for signal in state.constraint.signals():
+            # Add depended-on signals
+            set = {}
+            set['source'] = signal.name
+            # set['target'] = format_whitespace(state.name, "_", 2).replace("_", " ")
+            set['target'] = state.name
+            set['type'] = 'triggers'
+            sets.append(set)
+        if state.signal():
+            set = {}
+            # set['source'] = format_whitespace(state.name, "_", 2).replace("_", " ")
+            set['source'] = state.name
+            set['target'] = state.signal().name
+            set['type'] = 'emits'
+            sets.append(set)
+
+    for property_name, property in context._properties.items():
+        for signal in property.signals():
+            set = {}
+            set['source'] = str(property.id())
+            set['target'] = str(signal.name)
+            set['type'] = 'sets'
+            sets.append(set)
+
+
     return sets
-
-
-# background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print("Hello")
-    return "nothing"
 
 @socketio.on('update')
 def update():
@@ -70,4 +92,3 @@ def advertise(*, ctx, ip="0.0.0.0", port=5000, debug=False):
     app.debug=debug
     app.run(ip, port)
     socketio.run(app)
-    url_for('static', filename='graph.css')
