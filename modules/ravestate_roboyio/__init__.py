639def0fa93cca18ccd02fd2d47f45653487aa1f
realitivity <mail@josephbirkner.com>
2019-05-02 15:29:12 +0200

    Topic sub for STT, re-enabled spinning.

--- modules/ravestate_roboyio/__init__.py
+++ modules/ravestate_roboyio/__init__.py
@@ -2,6 +2,7 @@ from ravestate.module import Module
 from ravestate.state import state
 from ravestate.wrappers import ContextWrapper
 from ravestate_interloc import handle_single_interlocutor_input
+from ravestate_ros2.ros2_properties import Ros2SubProperty
 from ravestate.context import startup
 import ravestate_ros2
 
@@ -13,6 +14,8 @@ from time import sleep
 PYROBOY_AVAILABLE = False
 try:
     from pyroboy import say, listen, node
+    from roboy_cognition_msgs.msg import RecognizedSpeech
+    from roboy_cognition_msgs.srv import RecognizeSpeech
     PYROBOY_AVAILABLE = True
 except ImportError as e:
     logger.error(f"""
@@ -32,12 +35,24 @@ if PYROBOY_AVAILABLE:
 
     with Module(name="roboyio"):
 
-        @state(cond=startup(), read="interloc:all")
+        recognized_speech = Ros2SubProperty(
+            name="recognized_speech",
+            topic="/roboy/cognition/speech/recognition",
+            msg_type=RecognizedSpeech,
+            always_signal_changed=True)
+
+        @state(cond=recognized_speech.changed_signal(), read=("interloc:all", recognized_speech.id()))
         def roboy_input(ctx: ContextWrapper):
-            while not ctx.shutting_down():
-                result = listen()
-                if result:
-                    handle_single_interlocutor_input(ctx, result)
+            result = ctx[recognized_speech.id()]
+            if result:
+                handle_single_interlocutor_input(ctx, result)
+
+        # @state(cond=startup(), read="interloc:all")
+        # def roboy_input(ctx: ContextWrapper):
+        #     while not ctx.shutting_down():
+        #         result = listen()
+        #         if result:
+        #             handle_single_interlocutor_input(ctx, result)
 
         @state(read="rawio:out")
         def roboy_output(ctx):
