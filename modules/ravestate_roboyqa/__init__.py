d5617ed9c0a7017f773e6bbb196661de6b008a7c
Joseph Birkner <j.birkner@klebert-engineering.de>
2019-05-15 13:16:04 +0200

    Finished automatic creation of RoboyQA nodes.

--- modules/ravestate_roboyqa/__init__.py
+++ modules/ravestate_roboyqa/__init__.py
@@ -5,6 +5,9 @@ import ravestate_idle as idle
 import ravestate_rawio as rawio
 import ravestate_ontology
 
+from scientio.ontology.ontology import Ontology
+from scientio.session import Session
+from scientio.ontology.node import Node
 
 from os.path import realpath, dirname, join
 import random
@@ -15,10 +18,10 @@ logger = get_logger(__name__)
 
 verbaliser.add_folder(join(dirname(realpath(__file__)), "answering_phrases"))
 
-ROBOY_NODE_CONF_KEY = "roboy_node_id"
+ROBOY_NODE_PROP_CONF_KEY = "roboy_node_properties"
 
 
-with rs.Module(name="roboyqa", config={ROBOY_NODE_CONF_KEY: 356}):
+with rs.Module(name="roboyqa", config={ROBOY_NODE_PROP_CONF_KEY: {"name": "roboy two"}}):
 
     @rs.state(cond=idle.sig_bored, write=rawio.prop_out, weight=0.6, cooldown=30.)
     def hello_world_roboyqa(ctx):
@@ -53,13 +56,23 @@ with rs.Module(name="roboyqa", config={ROBOY_NODE_CONF_KEY: 356}):
         - what have you learned?
         - what are your abilities?
         """
-        sess = ravestate_ontology.get_session()
-        node_list = sess.retrieve(node_id=ctx.conf(key=ROBOY_NODE_CONF_KEY))
+        sess: Session = ravestate_ontology.get_session()
+        onto: Ontology = ravestate_ontology.get_ontology()
+
+        roboy = Node(metatype=onto.get_type("Robot"))
+        roboy.set_properties(ctx.conf(key=ROBOY_NODE_PROP_CONF_KEY))
+        node_list = sess.retrieve(request=roboy)
         if node_list:
             roboy = node_list[0]
         else:
-            logger.error(f"Seems like you do not have my memory running, or no node with ID {ctx.conf(key=ROBOY_NODE_CONF_KEY)} exists!")
-            return rs.Resign()
+            create_default_nodes()
+            node_list = sess.retrieve(request=roboy)
+            if node_list:
+                roboy = node_list[0]
+            else:
+                logger.error(f"Seems like you do not have my memory running, or no node with properties"
+                             f"{ctx.conf(key=ROBOY_NODE_PROP_CONF_KEY)} exists!")
+                return rs.Resign()
         
         triple = ctx[nlp.prop_triples][0]
 
@@ -133,68 +146,71 @@ with rs.Module(name="roboyqa", config={ROBOY_NODE_CONF_KEY: 356}):
 
 
 def create_default_nodes():
-    onto = get_ontology()
-    sess = get_session()
+    onto: Ontology = ravestate_ontology.get_ontology()
+    sess = ravestate_ontology.get_session()
 
     organization_type = onto.get_type("Organization")
     city_type = onto.get_type("City")
-    robot_type = onto.get_type("Robot")
     person_type = onto.get_type("Person")
     robot_type = onto.get_type("Robot")
     hobby_type = onto.get_type("Hobby")
 
     student_team_node = Node(metatype=organization_type)
     student_team_node.set_properties({
-        "name": "roboy student team"
-    })
+        "name": "roboy student team"})
+
     munich_node = Node(metatype=city_type)
     munich_node.set_properties({
-        "name": "munich"
-    })
+        "name": "munich"})
+
     roboy_junior_node = Node(metatype=robot_type)
     roboy_junior_node.set_properties({
         "name": "roboy",
         "abilities": "talking to people,reading wikipedia,reading reddit",
-        "skills": "tell jokes,tell fun facts about famous people and places,recognize you next time we meet,remember your name and our conversation",
+        "skills": "tell jokes,tell fun facts about famous people and places,recognize you next time we meet,"
+                  "remember your name and our conversation",
         "speed_kmh": 0,
         "full_name": "roboy junior",
         "birthdate": "08.03.2013",
         "conversation_id": "#bitch_boy",
         "sex": "male",
-        "age": 5
-    })
+        "age": 5})
+
     raf_node = Node(metatype=person_type)
     raf_node.set_properties({
         "name": "rafael",
-        "full_name": "rafael hostettler "
-    })
+        "full_name": "rafael hostettler"})
+
     lucy_node = Node(metatype=person_type)
     lucy_node.set_properties({
         "name": "lucy",
         "age": 6,
-        "sex": "female"
-    })
+        "sex": "female"})
+
     reddit_node = Node(metatype=hobby_type)
     reddit_node.set_properties({
-        "name": "reading reddit"
-    })
+        "name": "reading reddit"})
+
     roboy_node = Node(metatype=robot_type)
     roboy_node.set_properties({
         "name": "roboy two",
         "skills": "telling jokes,telling fun facts,telling you about the famous people and places,doing the math",
-        "abilities": "talk to people,recognize objects,show emotions,move my body,shake a hand,party like there is no tomorrow,surf the internet,answer your questions",
+        "abilities": "talk to people,recognize objects,show emotions,move my body,shake a hand,"
+                     "party like there is no tomorrow,surf the internet,answer your questions",
         "speed_kmh": 0,
         "birthdate": "12.04.2018",
         "full_name": "roboy 2.0",
         "conversation_id": "#bitch_boy",
-        "future": "become a robot rights advocate,help people and robots become friends,find the answer to the question of life the universe and everything,visit mars and other planets,become a music star,become a michelin star chef,get a robo pet,become as good as my father",
+        "future": "become a robot rights advocate,help people and robots become friends,"
+                  "find the answer to the question of life the universe and everything,"
+                  "visit mars and other planets,become a music star,become a michelin star chef,"
+                  "get a robo pet,become as good as my father",
         "sex": "male",
-        "age": "0"
-    })
-    tricycle_node = Node(metatype=Hobby)
+        "age": "0"})
+
+    tricycle_node = Node(metatype=hobby_type)
     tricycle_node.set_properties({
-        "name": "riding a tricycle"
-    })
+        "name": "riding a tricycle"})
 
     nodes = (
         student_team_node,
@@ -204,28 +220,24 @@ def create_default_nodes():
         lucy_node,
         reddit_node,
         roboy_node,
-        tricycle_node
-    )
+        tricycle_node)
     
     for node in nodes:
         sess.create(node)
 
-    roboy_junior_node.add_relationships( {"HAS_HOBBY": reddit_node.get_id()}       )
-    roboy_junior_node.add_relationships( {"MEMBER_OF": student_team_node.get_id()} )
-    roboy_junior_node.add_relationships( {"CHILD_OF":  raf_node.get_id()}          )
-    roboy_junior_node.add_relationships( {"LIVE_IN":   munich_node.get_id()}       )
-    raf_node.add_relationships(          {"FRIEND_OF": roboy_node.get_id()}        )
-    raf_node.add_relationships(          {"WORK_FOR":  student_team_node.get_id()} )
-    raf_node.add_relationships(          {"FRIEND_OF": roboy_junior_node.get_id()} )
-    lucy_node.add_relationships(         {"FRIEND_OF": roboy_node.get_id()}        )
-    lucy_node.add_relationships(         {"FRIEND_OF": roboy_junior_node.get_id()} )
-    roboy_node.add_relationships(        {"FROM":      munich_node.get_id()}       )
-    roboy_node.add_relationships(        {"SIBLING_OF":roboy_junior_node.get_id()} )
-    roboy_node.add_relationships(        {"HAS_HOBBY": tricycle_node.get_id()}     )
-    roboy_node.add_relationships(        {"MEMBER_OF": student_team_node.get_id()} )
-    roboy_node.add_relationships(        {"HAS_HOBBY": reddit_node.get_id()}       )
-    roboy_node.add_relationships(        {"CHILD_OF":  raf_node.get_id()}          )
-    roboy_node.add_relationships(        {"LIVE_IN":   munich_node.get_id()}       )
+    roboy_junior_node.add_relationships({"HAS_HOBBY":  {reddit_node.get_id()}})
+    roboy_junior_node.add_relationships({"MEMBER_OF":  {student_team_node.get_id()}})
+    roboy_junior_node.add_relationships({"CHILD_OF":   {raf_node.get_id()}})
+    roboy_junior_node.add_relationships({"LIVE_IN":    {munich_node.get_id()}})
+    raf_node.add_relationships({"FRIEND_OF":           {roboy_node.get_id(), roboy_junior_node.get_id()}})
+    raf_node.add_relationships({"WORK_FOR":            {student_team_node.get_id()}})
+    lucy_node.add_relationships({"FRIEND_OF":          {roboy_node.get_id(), roboy_junior_node.get_id()}})
+    roboy_node.add_relationships({"FROM":              {munich_node.get_id()}})
+    roboy_node.add_relationships({"SIBLING_OF":        {roboy_junior_node.get_id()}})
+    roboy_node.add_relationships({"HAS_HOBBY":         {tricycle_node.get_id(), reddit_node.get_id()}})
+    roboy_node.add_relationships({"MEMBER_OF":         {student_team_node.get_id()}})
+    roboy_node.add_relationships({"CHILD_OF":          {raf_node.get_id()}})
+    roboy_node.add_relationships({"LIVE_IN":           {munich_node.get_id()}})
 
     for node in nodes:
         sess.update(node)
