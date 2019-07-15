dc15ae182243ce8d7ab4236893372f5ae0ed65c3
Nikita Basargin <nbasargin@web.de>
2019-07-15 18:03:29 +0200

    proper scaling and positioning

--- modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
+++ modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnDestroy } from '@angular/core';
-import { MockDataService } from "../../mock-data/mock-data.service";
 import { Subscription } from "rxjs";
+
+import { MockDataService } from "../../mock-data/mock-data.service";
 import { NodeType } from "../elements/node.component";
 
 @Component({
     selector: 'app-activation-spike-graph',
     template: `
-        <svg>
-            <g class="moving-container" [style.transform]="'translateX(' + wrapperPosX + 'px)'">
+        <svg [attr.viewBox]="'0 0 ' + graphWidth + ' ' + graphHeight" preserveAspectRatio="xMidYMid meet">
+            <g class="moving-container" [style.transform]="'translateX(' + (containerPosX)  + 'px)'">
                 <g node *ngFor="let node of nodes" [x]="node.x" [y]="node.y" [label]="node.label"
                    [nodeType]="node.type"></g>
             </g>
@@ -19,19 +20,25 @@ export class ActivationSpikeGraphComponent implements OnDestroy {
 
     private dataSub: Subscription;
 
+    // internal graph size, defines the SVG coordinate system using viewBox
+    graphWidth = 1000;
+    graphHeight = 300;
+    // distance between two nodes (x-axis)
+    nodeSpacingX = 100;
+
     nodes: Array<{x: number, y: number, label: string, type: NodeType}> = [];
-    wrapperPosX = 0;
+    containerPosX = 0;  // translate whole container with a nice transition animation
 
     constructor(private mockDataService: MockDataService) {
         this.dataSub = mockDataService.dataStream.subscribe(data => {
             const newNode = {
-                x: 1000-this.wrapperPosX,
-                y: 150 + Math.floor(Math.random() * 200),
+                x: this.graphWidth - this.containerPosX,
+                y: Math.floor(Math.random() * this.graphHeight),
                 label: 'node ' + this.nodes.length,
                 type: this.nodes.length % 2 == 0 ? NodeType.SPIKE : NodeType.ACTIVATION
             };
             this.nodes.push(newNode);
-            this.wrapperPosX -= 100;
+            this.containerPosX -= this.nodeSpacingX;
         });
     }
 
