f9465b74174a326cadec9aaefa73d1bd31ed7d59
Nikita Basargin <nbasargin@web.de>
2019-07-15 19:06:40 +0200

    generating more nodes with connections

--- modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
+++ modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
@@ -9,8 +9,8 @@ import { NodeType } from "../elements/node.component";
     template: `
         <svg [attr.viewBox]="'0 0 ' + graphWidth + ' ' + graphHeight" preserveAspectRatio="xMidYMid meet">
             <g class="moving-container" [style.transform]="'translateX(' + (containerPosX)  + 'px)'">
-                <g node *ngFor="let node of nodes" [x]="node.x" [y]="node.y" [label]="node.label"
-                   [nodeType]="node.type"></g>
+                <g connector *ngFor="let c of connectors" [fromX]="c.fromX" [toX]="c.toX" [fromY]="c.fromY" [toY]="c.toY"></g>
+                <g node *ngFor="let node of nodes" [x]="node.x" [y]="node.y" [label]="node.label" [nodeType]="node.type"></g>
             </g>
         </svg>
         <div class="controls">
@@ -27,9 +27,10 @@ export class ActivationSpikeGraphComponent implements OnDestroy {
     graphWidth = 1000;
     graphHeight = 300;
     // distance between two nodes (x-axis)
-    nodeSpacingX = 100;
+    nodeSpacingX = 150;
 
     nodes: Array<{x: number, y: number, label: string, type: NodeType}> = [];
+    connectors: Array<{fromX: number, toX: number, fromY: number, toY: number}> = [];
     containerPosX = 0;  // translate whole container with a nice transition animation
 
     constructor(private mockDataService: MockDataService) {
@@ -37,20 +38,21 @@ export class ActivationSpikeGraphComponent implements OnDestroy {
 
         this.subscriptions.add(this.mockDataService.spikes.subscribe(spike => {
             console.log(spike);
+            const x = this.graphWidth - this.containerPosX;
+            const y = Math.random() * this.graphHeight;
+            const lastNode = this.nodes.length > 0 ? this.nodes[this.nodes.length - 1] : null;
+            this.addNode(x, y, NodeType.SPIKE, 'spike', lastNode);
+            this.containerPosX -= this.nodeSpacingX;
+
         }));
 
         this.subscriptions.add(this.mockDataService.ticks.subscribe(tick => {
-            console.log(tick);
-        }));
-
-        this.subscriptions.add(mockDataService.dataStream.subscribe(data => {
-            const newNode = {
-                x: this.graphWidth - this.containerPosX,
-                y: Math.floor(Math.random() * this.graphHeight),
-                label: 'node ' + this.nodes.length,
-                type: this.nodes.length % 2 == 0 ? NodeType.SPIKE : NodeType.ACTIVATION
-            };
-            this.nodes.push(newNode);
+            const x = this.graphWidth - this.containerPosX;
+            const y = Math.random() * (this.graphHeight - 200) + 100;
+            const lastNode = this.nodes.length > 0 ? this.nodes[this.nodes.length - 1] : null;
+            this.addNode(x, y - 70, NodeType.ACTIVATION, 'activation 1', lastNode);
+            this.addNode(x, y, NodeType.ACTIVATION, 'activation 2', lastNode);
+            this.addNode(x, y + 70, NodeType.ACTIVATION, 'activation 3', lastNode);
             this.containerPosX -= this.nodeSpacingX;
         }));
     }
@@ -62,5 +64,16 @@ export class ActivationSpikeGraphComponent implements OnDestroy {
     clear() {
         this.containerPosX = 0;
         this.nodes = [];
+        this.connectors = [];
+    }
+
+    addNode(x: number, y: number, type: NodeType, label: string, connectTo?: {x: number, y: number}) {
+        this.nodes.push({x, y, label, type});
+        if (connectTo) {
+            this.connectors.push({
+                fromX: connectTo.x, toX: x,
+                fromY: connectTo.y, toY: y
+            });
+        }
     }
 }
