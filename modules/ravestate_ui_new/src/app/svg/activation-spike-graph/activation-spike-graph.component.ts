8217068e2066d6dc0a2ee0e5d5640526ec7974f2
Nikita Basargin <nbasargin@web.de>
2019-07-15 17:16:33 +0200

    experimenting with animated shapes

--- modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
+++ modules/ravestate_ui_new/src/app/svg/activation-spike-graph/activation-spike-graph.component.ts
@@ -7,34 +7,31 @@ import { NodeType } from "../elements/node.component";
     selector: 'app-activation-spike-graph',
     template: `
         <svg>
-            <g node *ngFor="let node of nodes" [x]="node.x" [y]="node.y" [label]="node.label" [nodeType]="node.type"></g>
+            <g class="moving-container" [style.transform]="'translateX(' + wrapperPosX + 'px)'">
+                <g node *ngFor="let node of nodes" [x]="node.x" [y]="node.y" [label]="node.label"
+                   [nodeType]="node.type"></g>
+            </g>
         </svg>
     `,
     styleUrls: ['./activation-spike-graph.component.scss']
 })
 export class ActivationSpikeGraphComponent implements OnDestroy {
 
-    nodes: Array<{x: number, y: number, label: string, type: NodeType}> = [];
-
     private dataSub: Subscription;
+
+    nodes: Array<{x: number, y: number, label: string, type: NodeType}> = [];
+    wrapperPosX = 0;
 
     constructor(private mockDataService: MockDataService) {
         this.dataSub = mockDataService.dataStream.subscribe(data => {
-            console.log(data);
-
-            for (const node of this.nodes) {
-                node.x -= 100;
-            }
-
             const newNode = {
-                x: 500,
+                x: 1000-this.wrapperPosX,
                 y: 150 + Math.floor(Math.random() * 200),
                 label: 'node ' + this.nodes.length,
                 type: this.nodes.length % 2 == 0 ? NodeType.SPIKE : NodeType.ACTIVATION
             };
-
             this.nodes.push(newNode);
-
+            this.wrapperPosX -= 100;
         });
     }
 
