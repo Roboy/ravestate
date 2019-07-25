967cd12ad9068d99413aa9e0b2e84f8d2c0d1926
missxa <aliona.kharchenko@gmail.com>
2019-07-25 16:57:52 +0200

    fixes for study

--- modules/ravestate_roboyio/__init__.py
+++ modules/ravestate_roboyio/__init__.py
@@ -2,12 +2,12 @@ import ravestate as rs
 import ravestate_interloc as interloc
 import ravestate_rawio as rawio
 from unidecode import unidecode
-from threading import Lock
-from time import sleep
+from threading import RLock
+import time
 
 from reggol import get_logger
 logger = get_logger(__name__)
-
+import pdb
 
 PYROBOY_AVAILABLE = False
 try:
@@ -29,22 +29,34 @@ Please make sure to have the following items installed & sourced:
 
 if PYROBOY_AVAILABLE:
 
-    say_lock = Lock()
+    say_lock = RLock()
+    listen_start = 0
+    global say_end
+    say_end = 0
 
     with rs.Module(name="roboyio"):
 
         @rs.state(cond=rs.sig_startup, read=interloc.prop_all)
         def roboy_input(ctx: rs.ContextWrapper):
             while not ctx.shutting_down():
+                #with say_lock:
+                    #pdb.set_trace()
+                listen_start = time.time()
                 result = listen()
                 logger.info(f"pyroboy.listen() -> {result}")
-                if result:
+                global say_end
+                logger.info(f"listen_start: {listen_start}; say_end: {say_end}")
+                if result and listen_start > say_end:
                     interloc.handle_single_interlocutor_input(ctx, result)
-                sleep(1)  # LEDs are indicator of when roboy is listening
+                else:
+                    logger.info(f"Discarded {result}")
+                #time.sleep(1)  # LEDs are indicator of when roboy is listening
 
         @rs.state(read=rawio.prop_out)
         def roboy_output(ctx):
             # don't call say simultaneously in different threads
             with say_lock:
                 ret = say(unidecode(ctx[rawio.prop_out.changed()]))
+                global say_end
+                say_end = time.time()
             logger.info(f"pyroboy.say({ctx[rawio.prop_out.changed()]}) -> {ret}")
