512dc308440070bc4b0fcbae52afba7eaed4152f
Joseph Birkner <joseph.birkner@gmail.com>
2019-09-13 13:36:32 +0200

    Added UI activation updates.

--- modules/ravestate_ui_new/ui_context.py
+++ modules/ravestate_ui_new/ui_context.py
@@ -21,9 +21,9 @@ class UIContext(Context):
         self.sio = socketio.Server(cors_allowed_origins="*", async_mode="threading")
         self.next_id_for_object = defaultdict(int)
         self.ui_objects: Dict[Union[Spike, Activation], Union[UISpikeModel, UIActivationModel]] = dict()
-        Thread(target=self._serve_events_async).start()
+        Thread(target=self.ui_serve_events_async).start()
 
-    def _serve_events_async(self):
+    def ui_serve_events_async(self):
         app = flask.Flask(__name__)
         app.wsgi_app = socketio.Middleware(self.sio, app.wsgi_app)
         app.run(port=4242, threaded=True)
@@ -37,13 +37,11 @@ class UIContext(Context):
             new_obj = None
             if isinstance(spike_or_act, Spike):
                 new_obj = UISpikeModel(
-                    type="spike",
                     id=new_id,
                     signal=spike_or_act.id(),
                     parents=tuple(self.ui_model(parent).id for parent in parent_spikes))
             elif isinstance(spike_or_act, Activation):
                 new_obj = UIActivationModel(
-                    type="activation",
                     id=new_id,
                     state=spike_or_act.state_to_activate.name,
                     specificity=1.,
@@ -55,7 +53,55 @@ class UIContext(Context):
             return new_obj
 
     def ui_update_act(self, act: Activation, is_running=False):
-        pass
+        # -- determine whether the UI should be concerned with this
+        #  activation at all.
+        act_model = self.ui_model(act)
+        is_spiky = act.spiky()
+        if not is_spiky and act_model.status == "wait":
+            return
+        update_needed = False
+
+        # -- determine status transition
+        if is_running:
+            act_model.status = "run"
+            update_needed = True
+        else:
+            act_is_ready = act.constraint.evaluate()
+            if act_is_ready and act_model.status is "wait":
+                act_model.status = "ready"
+                update_needed = True
+
+        # -- build spike reference structure, while
+        #  determining whether the activations current one is up-to-date
+        new_spike_ref_struct = []
+        fully_unreferenced = True
+        for i, conj in enumerate(act.constraint.conjunctions()):
+            new_conj_dict = {}
+            cur_conj_dict = {}
+            new_spike_ref_struct.append(new_conj_dict)
+            if len(act_model.spikes) > i:
+                cur_conj_dict = act_model.spikes[i]
+            else:
+                update_needed = True
+            for sig in conj.signals():
+                spike_id = -1
+                if sig.spike:
+                    spike_id = self.ui_model(sig.spike).id
+                    fully_unreferenced = False
+                new_conj_dict[sig.name] = spike_id
+                if sig.name not in cur_conj_dict or cur_conj_dict[sig.name] != spike_id:
+                    update_needed = True
+
+        # -- update spike ref structure, send activation to frontend on-demand
+        if update_needed:
+            act_model.spikes = new_spike_ref_struct
+            self.sio.emit("activation", vars(act_model))
+
+        # -- forget activation, if it was run and it is fully unreferenced
+        if fully_unreferenced and act_model.status is "run":
+            del self.ui_objects[act]
+
+    # --------------------- Context Overrides ---------------------
 
     def emit(self, signal, parents=None, wipe: bool=False, payload=None) -> None:
         # Copy instead of calling super to obtain reference to created spike.
@@ -72,6 +118,14 @@ class UIContext(Context):
             new_spike_model = self.ui_model(new_spike, parent_spikes=parents if parents else ())
             self.sio.emit("spike", vars(new_spike_model))
 
+    def run_once(self, seconds_passed=1., debug=False):
+        super(UIContext, self).run_once(seconds_passed, debug)
+        acts_to_update = (
+            {act for act in self.ui_objects if isinstance(act, Activation)} |
+            self._state_activations())
+        for act in acts_to_update:
+            self.ui_update_act(act)
+
     def _state_activated(self, act: Activation):
         super(UIContext, self)._state_activated(act)
-        self.ui_update_act(act)
+        self.ui_update_act(act, is_running=True)
--- modules/ravestate_ui_new/ui_model.py
+++ modules/ravestate_ui_new/ui_model.py
@@ -4,8 +4,8 @@ class UIActivationModel:
     """
     Model which represents a spike, as seen by the ravestate UI.
     """
-    def __init__(self, *, type, id, state, specificity, status, spikes):
-        self.type = type
+    def __init__(self, *, id, state, specificity, status, spikes):
+        self.type = "activation"
         self.id = id
         self.state = state
         self.specificity = specificity
@@ -17,8 +17,8 @@ class UISpikeModel:
     """
     Model which represents an activation, as seen by the ravestate UI.
     """
-    def __init__(self, type, id, signal, parents):
-        self.type = type
+    def __init__(self, *, id, signal, parents):
+        self.type = "spike"
         self.id = id
         self.signal = signal
         self.parents = parents
