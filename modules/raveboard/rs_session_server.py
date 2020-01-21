2a29be19f664664971793253b9c6a202db1a9b5e
Joseph Birkner <j.birkner@klebert-engineering.de>
2020-01-21 20:38:49 +0100

    Intro of token concept.

--- modules/raveboard/rs_session_server.py
+++ modules/raveboard/rs_session_server.py
@@ -1,12 +1,33 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from typing import Dict, List
+app = Flask(__name__, static_folder="dist/ravestate")
+
+# Secret tokens per socketio-uri
+current_tokens: Dict[str, str] = {}
+
+# Ports for which no active ravestate instance is running
+free_ports: List[int] = list(range(5001, 5010))
 
 
 @app.route('/')
 def hello():
-    # Take a free port
-    # Start ravestate on the selected port
-    # Render template with port filled in
+
+    if "rs-sio-uri" in request.args and "token" in request.args:
+
+        rs_sio_uri = request.args["rs-sio-uri"]
+        token = request.args["token"]
+
+        if rs_sio_uri in current_tokens and current_tokens[rs_sio_uri] == token:
+
+            return
+
+    # take a free port
+
+    # start ravestate on the selected port
+    # optional: Create a secret that must be known by port users.
+
+    # forward with rs-sio-uri & token
     pass
 
 
