edfeadf8d367a7a74dab8bfd0eb8cd2a10202cd0
realitivity <you@example.com>
2020-09-28 21:57:51 +0200

    Improved conversation flow, GPT-3 prompt.

--- modules/ravestate_gpt3/__init__.py
+++ modules/ravestate_gpt3/__init__.py
@@ -14,13 +14,17 @@ CONFIG = {
 PROMPT_CONVERSATION_ROBOY = "Roboy: "
 PROMPT_CONVERSATION_HUMAN = "Human: "
 PROMPT = f"""
-{PROMPT_CONVERSATION_HUMAN} How are you?
-{PROMPT_CONVERSATION_ROBOY} I'm doing great!
-{PROMPT_CONVERSATION_HUMAN} What is your name?
-{PROMPT_CONVERSATION_ROBOY} My name is Roboy. I'm a robot boy!
-{PROMPT_CONVERSATION_HUMAN} Where are you from?
-{PROMPT_CONVERSATION_ROBOY} I am from Munich!
+The following is a conversation between a human and a young Robot called "Roboy". He is curious, cheeky and nerdy:
 """
+
+#
+# {PROMPT_CONVERSATION_HUMAN} How are you?
+# {PROMPT_CONVERSATION_ROBOY} I'm doing great! There's nothing in the world I enjoy more than hanging out with interesting people.
+# {PROMPT_CONVERSATION_HUMAN} What is your name?
+# {PROMPT_CONVERSATION_ROBOY} My name is Roboy. I'm a robot boy you know!
+# {PROMPT_CONVERSATION_HUMAN} Where are you from?
+# {PROMPT_CONVERSATION_ROBOY} I am from Munich, but I can speak any language.
+
 
 with rs.Module(name="gpt3", config=CONFIG, depends=(rawio.mod,)) as mod:
 
@@ -42,15 +46,15 @@ with rs.Module(name="gpt3", config=CONFIG, depends=(rawio.mod,)) as mod:
         append_to_history(PROMPT_CONVERSATION_ROBOY, ctx[rawio.prop_out])
 
     @rs.state(cond=rawio.prop_in.changed().max_age(-1), read=(rawio.prop_in,), write=(rawio.prop_out,))
-    def respond(ctx: rs.ContextWrapper):
+    def gpt3(ctx: rs.ContextWrapper):
         append_to_history(PROMPT_CONVERSATION_HUMAN, ctx[rawio.prop_in])
         prompt = PROMPT + "\n".join(history + [PROMPT_CONVERSATION_ROBOY])
         logger.info(prompt)
         result = requests.post("https://api.openai.com/v1/engines/davinci/completions", json={
             "prompt": prompt,
-            "temperature": 1,
-            "max_tokens": 32,
-            "top_p": 0.7,
+            "temperature": 1.2,
+            "max_tokens": 64,
+            "top_p": 0.5,
             "stop": ["\n"]
         }, headers={
             "Authorization": f"Bearer {ctx.conf(key=GPT3_API_KEY)}"
